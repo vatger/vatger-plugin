d3d07b5c6dfec849f163f48df66fe074de342b34
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-06-14 22:06:02 +0200

    plugin: LoggerAsyncBase - apply style guide

--- plugin/src/log/LoggerAsyncBase.h
+++ plugin/src/log/LoggerAsyncBase.h
@@ -9,6 +9,7 @@
 #include <source_location>
 #include <string>
 #include <thread>
+#include <utility>
 
 #include "ILogger.h"
 
@@ -24,60 +25,60 @@ class LoggerAsyncBase : public ILogger {
 
     virtual void emitLog(const LogMessage& logMsg) = 0;
 
-    std::queue<LogMessage> m_queue;
-    std::mutex m_mutex;
-    std::condition_variable m_cv;
-    std::thread m_worker;
-    std::atomic<bool> m_running;
+    std::queue<LogMessage> m_queue_;
+    std::mutex m_mutex_;
+    std::condition_variable m_cv_;
+    std::thread m_worker_;
+    std::atomic<bool> m_running_;
 
-    std::condition_variable m_flushCv;
+    std::condition_variable m_flush_cv_;
 
     void processLogs() {
-        while (m_running || !m_queue.empty()) {
-            std::unique_lock<std::mutex> lock(m_mutex);
-            m_cv.wait(lock, [this]() { return !m_queue.empty() || !m_running; });
+        while (m_running_ || !m_queue_.empty()) {
+            std::unique_lock<std::mutex> lock(m_mutex_);
+            m_cv_.wait(lock, [this]() { return !m_queue_.empty() || !m_running_; });
 
-            while (!m_queue.empty()) {
-                auto logMsg = std::move(m_queue.front());
-                m_queue.pop();
+            while (!m_queue_.empty()) {
+                auto log_msg = std::move(m_queue_.front());
+                m_queue_.pop();
                 lock.unlock();
 
-                this->emitLog(logMsg);
+                this->emitLog(log_msg);
 
                 lock.lock();
             }
 
-            m_flushCv.notify_all();
+            m_flush_cv_.notify_all();
         }
     }
 
    public:
-    LoggerAsyncBase() : m_running(true), m_worker(&LoggerAsyncBase::processLogs, this) {}
+    LoggerAsyncBase() : m_running_(true), m_worker_(&LoggerAsyncBase::processLogs, this) {}
     virtual ~LoggerAsyncBase() {
         {
-            std::unique_lock<std::mutex> lock(m_mutex);
-            m_running = false;
-            m_cv.notify_all();  // notify worker thread to exit
+            std::unique_lock<std::mutex> lock(m_mutex_);
+            m_running_ = false;
+            m_cv_.notify_all();  // notify worker thread to exit
         }
         {
-            std::unique_lock<std::mutex> lock(m_mutex);
-            m_flushCv.wait(lock, [this]() { return m_queue.empty(); });
+            std::unique_lock<std::mutex> lock(m_mutex_);
+            m_flush_cv_.wait(lock, [this]() { return m_queue_.empty(); });
         }
 
-        if (m_worker.joinable()) {
-            m_worker.join();
+        if (m_worker_.joinable()) {
+            m_worker_.join();
         }
-    };
+    }
 
     void LoggerAsyncBase::log(const LogLevel level, const std::string& message,
                               const std::source_location location = std::source_location::current()) {
         LogMessage logMsg{level, message, location, std::chrono::system_clock::now()};
 
         {
-            std::lock_guard<std::mutex> lock(m_mutex);
-            m_queue.push(std::move(logMsg));
+            std::lock_guard<std::mutex> lock(m_mutex_);
+            m_queue_.push(std::move(logMsg));
         }
-        m_cv.notify_one();
+        m_cv_.notify_one();
     }
 };
 }  // namespace logging
