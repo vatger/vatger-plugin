ca5262fcb35284c870d17c36a689d2be694845af
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-07-22 21:13:37 +0200

    remove service/

--- service/README.md
+++
@@ -1,5 +0,0 @@
-### Starting service
-
-in service/src/:
-```uv run taskiq worker app.tkq:broker app.tasks --reload```
-```uv run taskiq scheduler app.broker:scheduler```
--- service/src/interfaces/gateways/datafeed_gateway_interface.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-from models.datafeed import DatafeedModel
-
-
-class DatafeedGatewayInterface(ABC):
-    @abstractmethod
-    async def fetch(self) -> DatafeedModel | None: ...
